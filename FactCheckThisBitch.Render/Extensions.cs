94d63a5a41ac53b72c06e64cee6dc5fc8541eae6
Theo Kandiliotis <Theo.Kandiliotis@BritishCouncil.Org>
2021-12-16 14:25:47 +0000

    Fixed puzzle and keyword render imperfections

--- FactCheckThisBitch.Render/Extensions.cs
+++ FactCheckThisBitch.Render/Extensions.cs
@@ -8,99 +8,211 @@ namespace FactCheckThisBitch.Render
 {
     public static class Extensions
     {
-        public static string GetKeywordsText(this Piece piece)
+        public static string GetKeywordsText(this Piece piece,int skip=0,int take=100)
         {
-            var result = string.Join(Environment.NewLine, piece.Keywords.Select(k => $"• {k.Trim()}"));
+            var result = string.Join(Environment.NewLine,
+                piece.Keywords.Skip(skip).Take(take).Select(k => $"• {k.KeywordFormat()}"));
             return result;
         }
 
-        public static dynamic GetPieceCoordinates(this Puzzle puzzle, int pieceIndex)
+        private static string KeywordFormat(this string keyword)
+        {
+            keyword = keyword.Trim();
+            if (keyword.Length > 13)
+            {
+                return keyword.Replace(" ",
+                    Environment.NewLine);
+            }
+
+            return keyword;
+        }
+
+        public static dynamic GetPieceCoordinates(this Puzzle puzzle,
+            int pieceIndex)
         {
             switch (pieceIndex)
             {
                 case 1:
                     return new
                     {
-                        X = 17, Y = 11, KeywordsBox = new
+                        X = 17,
+                        Y = 11,
+                        KeywordsBoxes = new dynamic[]
                         {
-                            Left = 0,
-                            Top = 0,
-                            Right = 250,
-                            Bottom = 157
+                            new
+                            {
+                                Left = 0,
+                                Top = 0,
+                                Right = 250,
+                                Bottom = 157
+                            }
                         }
                     };
                 case 2:
-                    return new {X = 280, Y = 11, KeywordsBox = new
+                    return new
+                    {
+                        X = 280,
+                        Y = 11,
+                        KeywordsBoxes = new dynamic[]
                         {
-                            Left = 0,
-                            Top = 0,
-                            Right = 250,
-                            Bottom = 157
-                        }};
+                            new
+                            {
+                                Left = 0,
+                                Top = 0,
+                                Right = 250,
+                                Bottom = 157
+                            }
+                        }
+
+                    };
                 case 3:
-                    return new {X = 449, Y = 11, KeywordsBox = new
+                    return new
                     {
-                        Left = 95,
-                        Top = 0,
-                        Right = 344,
-                        Bottom = 156
-                    }};
+                        X = 449,
+                        Y = 11,
+                        KeywordsBoxes = new dynamic[]
+                        {
+                            new
+                            {
+                                Left = 95,
+                                Top = 0,
+                                Right = 344,
+                                Bottom = 156
+                            }
+                        }
+
+                    };
                 case 4:
-                    return new {X = 17, Y = 182, KeywordsBox = new
+                    return new
                     {
-                        Left = 0,
-                        Top = 96,
-                        Right = 250,
-                        Bottom = 173
-                    }};
+                        X = 17,
+                        Y = 182,
+                        KeywordsBoxes = new dynamic[]
+                        {
+                            new
+                            {
+                                Left = 0,
+                                Top = 96,
+                                Right = 253,
+                                Bottom = 174
+                            },
+                            new
+                            {
+                                Left = 0,
+                                Top = 274,
+                                Right = 253,
+                                Bottom = 349
+                            }
+                        }
+
+                    };
                 case 5:
-                    return new {X = 186, Y = 182, KeywordsBox = new
+                    return new
                     {
-                        Left = 99,
-                        Top = 99,
-                        Right = 344,
-                        Bottom = 346
-                    }};
+                        X = 186,
+                        Y = 181,
+                        KeywordsBoxes = new dynamic[]
+                        {
+                            new
+                            {
+                                Left = 99,
+                                Top = 99,
+                                Right = 344,
+                                Bottom = 346
+                            }
+                        }
+
+                    };
                 case 6:
-                    return new {X = 544, Y = 182, KeywordsBox = new
+                    return new
                     {
-                        Left = 1,
-                        Top = 99,
-                        Right = 248,
-                        Bottom = 172
-                    }};
+                        X = 544,
+                        Y = 181,
+                        KeywordsBoxes = new dynamic[]
+                        {
+                            new
+                            {
+                                Left = 0,
+                                Top = 94,
+                                Right = 253,
+                                Bottom = 174
+                            },
+                            new
+                            {
+                                Left = 0,
+                                Top = 267,
+                                Right = 253,
+                                Bottom = 352
+                            }
+                        }
+
+                    };
                 case 7:
-                    return new {X = 18, Y = 543, KeywordsBox = new
+                    return new
                     {
-                        Left = 2,
-                        Top = 98,
-                        Right = 246,
-                        Bottom = 250
-                    }};
+                        X = 18,
+                        Y = 543,
+                        KeywordsBoxes = new dynamic[]
+                        {
+                            new
+                            {
+                                Left = 2,
+                                Top = 98,
+                                Right = 246,
+                                Bottom = 250
+                            }
+                        }
+
+                    };
                 case 8:
-                    return new {X = 280, Y = 543, KeywordsBox = new
+                    return new
                     {
-                        Left = 3,
-                        Top = 183,
-                        Right = 248,
-                        Bottom = 251
-                    }};
+                        X = 280,
+                        Y = 543,
+                        KeywordsBoxes = new dynamic[]
+                        {
+                            new
+                            {
+                                Left = 3,
+                                Top = 176,
+                                Right = 248,
+                                Bottom = 255
+                            }
+                        }
+                    };
                 case 9:
-                    return new {X = 449, Y = 543, KeywordsBox = new
+                    return new
                     {
-                        Left = 97,
-                        Top = 97,
-                        Right = 342,
-                        Bottom = 248
-                    }};
+                        X = 449,
+                        Y = 543,
+                        KeywordsBoxes = new dynamic[]
+                        {
+                            new
+                            {
+                                Left = 97,
+                                Top = 97,
+                                Right = 342,
+                                Bottom = 248
+                            }
+                        }
+
+                    };
                 default:
-                    return new {X = 50, Y = 50, KeywordsBox = new
+                    return new
                     {
-                        Left = 0,
-                        Top = 0,
-                        Right = 0,
-                        Bottom = 0
-                    }};
+                        X = 50,
+                        Y = 50,
+                        KeywordsBoxes = new dynamic[]
+                        {
+                            new
+                            {
+                                Left = 0,
+                                Top = 0,
+                                Right = 0,
+                                Bottom = 0
+                            }
+                        }
+                    };
             }
         }
     }
--- FactCheckThisBitch.Render/PuzzleRenderer.cs
+++ FactCheckThisBitch.Render/PuzzleRenderer.cs
@@ -40,13 +40,7 @@ namespace FactCheckThisBitch.Render
 
         private void OpenOutputFolder()
         {
-            new Process
-            {
-                StartInfo = new ProcessStartInfo(_outputFolder)
-                {
-                    UseShellExecute = true
-                }
-            }.Start();
+            new Process {StartInfo = new ProcessStartInfo(_outputFolder) {UseShellExecute = true}}.Start();
         }
 
         private void PrepareOutputFolder()
@@ -99,8 +93,21 @@ namespace FactCheckThisBitch.Render
                     Font font = SystemFonts.CreateFont("Arial", 10); // for scaling water mark size is largely ignored.
                     using (var pieceImageWithKeywords = pieceImage.Clone(ctx =>
                     {
-                        ImageSharpExtensions.ApplyScalingWaterMark(ctx, font, puzzlePiece.Piece.GetKeywordsText(),
-                            pieceCoordinates.KeywordsBox, Color.White, 5, false);
+                        var keywordsBoxes = pieceCoordinates.KeywordsBoxes as dynamic[];
+                        if (keywordsBoxes.Length > 1 && puzzlePiece.Piece.Keywords.Count>1)
+                        {
+                            ImageSharpExtensions.ApplyScalingWaterMark(ctx, font, puzzlePiece.Piece.GetKeywordsText(0,1),
+                                keywordsBoxes[0], Color.White, 5, false);
+
+                            ImageSharpExtensions.ApplyScalingWaterMark(ctx, font, puzzlePiece.Piece.GetKeywordsText(1),
+                                keywordsBoxes[1], Color.White, 5, false);
+                        }
+                        else
+                        {
+                            ImageSharpExtensions.ApplyScalingWaterMark(ctx, font, puzzlePiece.Piece.GetKeywordsText(),
+                                keywordsBoxes[0], Color.White, 5, false);
+                        }
+
                     }))
                     {
                         pieceImageWithKeywords.Save(pieceImgeWithKeywordsPath);
@@ -114,95 +121,88 @@ namespace FactCheckThisBitch.Render
             var templatePath = Path.Combine(_assetsFolder, "Template.pptx");
             using (IPresentation doc = Presentation.Open(templatePath))
             {
-
                 #region introduction slide
+
                 var puzzleSlide = doc.Slides[0];
                 puzzleSlide.GroupShape("puzzle_metadata").UpdateText("puzzle_title", _puzzle.Title);
                 puzzleSlide.GroupShape("puzzle_metadata").UpdateText("puzzle_thesis", _puzzle.Thesis);
-
 
                 #endregion
 
                 var pieceSlideTemplate = doc.Slides[1];
                 var pieceReferencesTemplate = doc.Slides[2];
-
+                var conslusionSlideTemplate = doc.Slides[3];
+                var conclusionSlide = conslusionSlideTemplate.Clone();
 
                 #region create one slide for each piece
+
                 foreach (var puzzlePiece in _puzzle.PuzzlePieces)
                 {
                     var newPieceSlide = pieceSlideTemplate.Clone();
                     newPieceSlide.Name = puzzlePiece.Piece.Id;
                     newPieceSlide.SlideTransition.TransitionEffect = TransitionEffect.None;
-
-
                     newPieceSlide.GroupShape("piece_metadata").UpdateText("piece_title", puzzlePiece.Piece.Title);
                     newPieceSlide.GroupShape("piece_metadata").UpdateText("piece_thesis", puzzlePiece.Piece.Thesis);
 
                     //replace puzzle picture
-                    newPieceSlide.ReplacePicture("empty_puzzle", Path.Combine(_outputFolder, $"Puzzle{puzzlePiece.Index - 1}.png"));
+                    newPieceSlide.ReplacePicture("empty_puzzle",
+                        Path.Combine(_outputFolder, $"Puzzle{puzzlePiece.Index - 1}.png"));
 
                     //replace piece picture
-                    newPieceSlide.ReplacePicture("puzzle_piece", Path.Combine(_outputFolder, $"Piece{puzzlePiece.Index}_Keywords.png"));
+                    newPieceSlide.ReplacePicture("puzzle_piece",
+                        Path.Combine(_outputFolder, $"Piece{puzzlePiece.Index}_Keywords.png"));
 
                     #region position puzzle piece inside puzzle
+
                     var pieceCoordinates = _puzzle.GetPieceCoordinates(puzzlePiece.Index);
                     var puzzlePicture = newPieceSlide.Shapes.First(p => p.ShapeName == "empty_puzzle");
                     var puzzlePieceShape = newPieceSlide.Shapes.First(s => s.ShapeName == "puzzle_piece");
-
                     double scaleRatioOfPuzzle = 0.88;
-                    var pieceLeft = puzzlePicture.Left + ((double)pieceCoordinates.X * scaleRatioOfPuzzle).PixelsToPoints();
-                    var pieceTop = puzzlePicture.Top + ((double)pieceCoordinates.Y * scaleRatioOfPuzzle).PixelsToPoints();
+                    var pieceLeft = puzzlePicture.Left + ((double) pieceCoordinates.X * scaleRatioOfPuzzle).PixelsToPoints();
+                    var pieceTop = puzzlePicture.Top + ((double) pieceCoordinates.Y * scaleRatioOfPuzzle).PixelsToPoints();
                     using (var pieceImage = Image.Load(Path.Combine(_outputFolder, $"Piece{puzzlePiece.Index}_Keywords.png")))
                     {
-                        var pieceImageWidth = ((double)pieceImage.Width * scaleRatioOfPuzzle).PixelsToPoints();
-                        var pieceImageHeight = ((double)pieceImage.Height * scaleRatioOfPuzzle).PixelsToPoints();
+                        var pieceImageWidth = ((double) pieceImage.Width * scaleRatioOfPuzzle).PixelsToPoints();
+                        var pieceImageHeight = ((double) pieceImage.Height * scaleRatioOfPuzzle).PixelsToPoints();
                         puzzlePieceShape.Left = pieceLeft;
                         puzzlePieceShape.Top = pieceTop;
                         puzzlePieceShape.Width = pieceImageWidth;
                         puzzlePieceShape.Height = pieceImageHeight;
                     }
+
                     #endregion
 
                     doc.Slides.Add(newPieceSlide);
 
                     #region Create a slide for each reference of this piece
 
-
                     var referenceIndex = 0;
                     foreach (var reference in puzzlePiece.Piece.References)
                     {
-
-
                         foreach (string image in reference.Images)
                         {
-
                             var referenceSlide = pieceReferencesTemplate.Clone();
                             referenceSlide.Name = $"{reference.Id}-{image}";
                             var slideSequence = referenceSlide.Timeline.MainSequence;
-
                             var referenceGroupShape = referenceSlide.GroupShape("group_article");
                             referenceGroupShape.UpdateText("textbox_url", reference.Url.Limit(100));
-                            referenceGroupShape.ReplacePicture("picture_screenshot",
-                                Path.Combine(_mediaFolder, image)); //todo: handle multiple images
-
-                            //todo: if it's not the first reference of the piece, remove the tv picture and set
-                            //the slide transition to fade
+                            referenceGroupShape.UpdateText("textbox_source",
+                                reference.Source.IsEmpty() ? "" : $"Source: {reference.Source}");
+                            referenceGroupShape.UpdateText("textbox_date",
+                                (!reference.DatePublished.HasValue)
+                                    ? ""
+                                    : $"Date Published: {reference.DatePublished.Value:dd/MM/yyyy}");
+                            referenceGroupShape.ReplacePicture("picture_screenshot", Path.Combine(_mediaFolder, image));
                             if (referenceIndex > 0)
                             {
-                                
                                 slideSequence.RemoveByShape(referenceGroupShape as IShape);
                                 referenceSlide.Pictures.RemoveAt(0);
                             }
 
                             doc.Slides.Add(referenceSlide);
-
                             referenceIndex++;
-
                         }
-
-
                     }
-
 
                     #endregion
                 }
@@ -211,7 +211,14 @@ namespace FactCheckThisBitch.Render
 
                 doc.Slides.Remove(pieceSlideTemplate);
                 doc.Slides.Remove(pieceReferencesTemplate);
+                doc.Slides.Remove(conslusionSlideTemplate);
 
+                #region conclusion slide
+
+                conclusionSlide.GroupShape("puzzle_conclusion").UpdateText("conclusion_text", _puzzle.Conclusion);
+                doc.Slides.Add(conclusionSlide);
+
+                #endregion
 
                 doc.Save(Path.Combine(_outputFolder, $"{_puzzle.Title.ToSanitizedString()}.pptx"));
             }
@@ -223,26 +230,25 @@ namespace FactCheckThisBitch.Render
             using (IPresentation doc = Presentation.Open(puzzlePresentationPath))
             {
                 doc.Slides[0].SlideTransition.TimeDelay = _puzzle.Duration;
-
                 foreach (var puzzlePiece in _puzzle.PuzzlePieces)
                 {
                     bool isFirstImageInPiece = true;
-
                     doc.Slides.First(s => s.Name == puzzlePiece.Piece.Id).SlideTransition.TimeDelay = puzzlePiece.Piece.Duration;
                     foreach (var reference in puzzlePiece.Piece.References)
                     {
                         foreach (var image in reference.Images)
                         {
-                            var slide = doc.Slides.First(s=>s.Name == $"{reference.Id}-{image}");
-                            
-
+                            var slide = doc.Slides.First(s => s.Name == $"{reference.Id}-{image}");
                             if (!isFirstImageInPiece)
                             {
                                 slide.SlideTransition.TransitionEffect = TransitionEffect.FadeAway;
                                 slide.SlideTransition.Duration = 0.4f;
-
+                                slide.SlideTransition.TimeDelay = reference.Duration - 0.5f;
                             }
-                            slide.SlideTransition.TimeDelay = reference.Duration;
+                            else
+                            {
+                                slide.SlideTransition.TimeDelay = reference.Duration;
+                            }
 
                             isFirstImageInPiece = false;
                         }
@@ -251,7 +257,6 @@ namespace FactCheckThisBitch.Render
 
                 doc.Save(puzzlePresentationPath);
             }
-
         }
 
         public void Dispose()
