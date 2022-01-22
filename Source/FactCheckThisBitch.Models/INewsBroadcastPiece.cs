0614915c0f0fb60ae0112dcfe43e7de79bba346a
Theo Kandiliotis <tkandiliotis@gmail.com>
2022-01-22 02:42:39 +0000

    VideoEditing;large tv in news broadcast

---
+++ Source/FactCheckThisBitch.Models/INewsBroadcastPiece.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactCheckThisBitch.Models
+{
+
+    public interface INewsBroadcastSegment
+    {
+        //public IEnumerable<string> ScrollingText;
+    }
+}
